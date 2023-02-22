c72d6585aebb367dc357f7c0521a3784313c3f10
kimlg2015@naver,com <kimlg2015@naver.com>
2023-02-22 22:18:12 +0900

    눈썹 문신

--- app/src/main/java/com/example/eybrow/MainActivity.java
+++ app/src/main/java/com/example/eybrow/MainActivity.java
@@ -27,11 +27,10 @@ import com.example.eybrow.Fragment.FragmentCallback;
 import com.example.eybrow.Register.LoginActivity;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity
-    implements NavigationView.OnNavigationItemSelectedListener,FragmentCallback {
+public class MainActivity extends AppCompatActivity {
 
     private TextView slide_nametext1;
-    private String userID;
+    private String userID, username;
     private Button btnLogout;
     private ImageView iv_menu;
     private DrawerLayout drawerLayout;
@@ -41,11 +40,11 @@ public class MainActivity extends AppCompatActivity
     Fragment3 fragment3;
     Fragment4 fragment4;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -53,98 +52,19 @@ public class MainActivity extends AppCompatActivity
         slide_nametext1 = (TextView) findViewById(R.id.slide_nametext1);
 //        iv_menu = (ImageView) findViewById(R.id.iv_menu);
         drawerLayout = findViewById(R.id.drawer_layout);
-//        Intent intent = getIntent();
-//
-//        String username = intent.getStringExtra("username");
-//        slide_nametext1.setText(username.toString() + "님 환영합니다!!");
-//        slide_nametext1.setText(username);
-//
-//        iv_menu.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = getIntent();
-//
-//                String username = intent.getStringExtra("username");
-//                slide_nametext1.setText(username.toString() + "님 환영합니다!!");
-//                slide_nametext1.setText(username);
-//            }
-//
-//        });
+        iv_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = getIntent();
 
+                String username = intent.getStringExtra("username");
+                slide_nametext1.setText(username.toString() + "님 환영합니다!!");
+                slide_nametext1.setText(username);
+            }
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-        fragment1 = new Fragment1();
-        fragment2 = new Fragment2();
-        fragment3 = new Fragment3();
-        fragment4 = new Fragment4();
-        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment1).commit();
-
-
+        });
     }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.nav_home) {
-            Toast.makeText(this, "첫번째 메뉴 선택됨.", Toast.LENGTH_LONG).show();
-            onFragmentSelected(0, null);
-        } else if (id == R.id.nav_reservation) {
-            Toast.makeText(this, "두번째 메뉴 선택됨.", Toast.LENGTH_LONG).show();
-            onFragmentSelected(1, null);
-        } else if (id == R.id.nav_board) {
-            Toast.makeText(this, "세번째 메뉴 선택됨.", Toast.LENGTH_LONG).show();
-            onFragmentSelected(2, null);
-        } else if (id == R.id.nav_Favorites) {
-            Toast.makeText(this, "네번째 메뉴 선택됨.", Toast.LENGTH_LONG).show();
-            onFragmentSelected(3, null);
-        }
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-
-    @Override
-    public void onFragmentSelected(int position, Bundle bundle) {
-        Fragment curFragment = null;
-
-        if (position == 0) {
-            curFragment = fragment1;
-            toolbar.setTitle("첫번째 화면");
-        } else if (position == 1) {
-            curFragment = fragment2;
-            toolbar.setTitle("두번째 화면");
-        } else if (position == 2) {
-            curFragment = fragment3;
-            toolbar.setTitle("세번째 화면");
-        } else if (position == 3) {
-            curFragment = fragment4;
-            toolbar.setTitle("네번째 화면");
-        }
-            // curFragment 화면으로 변경시킨다.
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, curFragment).commit();
-        }
-
-
-
-    }
+}
 
 //        btnLogout.setOnClickListener(new View.OnClickListener() {
 //            @Override
